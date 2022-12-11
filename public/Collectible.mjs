fd1a59c93c1897cefe0245022d7d04a7e9275731
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-12-11 03:33:47 +0000

    player setup started

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,6 +1,6 @@
 class Collectible {
   constructor({x, y, value, id}) {
-
+  
   }
 
 }
--- public/Player.mjs
+++ public/Player.mjs
@@ -1,14 +1,31 @@
 class Player {
   constructor({x, y, score, id}) {
-    return "";
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
+    this.speed = 5;
+    this.score = score;
+    this.id = id;
+    this.movementDirection = {};
+    this.isMain = main;
   }
 
   movePlayer(dir, speed) {
-
+    switch(dir){
+      case "UP":
+        this.y -= 1;
+      case "DOWN":
+        this.y += 1;
+      case "LEFT":
+        this.x -= 1;
+      case "RIGHT":
+        this.x += 1;
+    }
   }
 
   collision(item) {
-
+    
   }
 
   calculateRank(arr) {
